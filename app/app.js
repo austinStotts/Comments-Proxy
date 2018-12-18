9eed1a05eb62de1fb5dbb2b5f0fbb3cc6c480466
Austin Stotts <austintstotts@gmail.com>
2018-12-18 16:26:46 -0600

    webpack / babel / npm setup complete

---
+++ app/app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>{'hello :)'}</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
