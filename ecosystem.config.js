d1e67baa50d8bb975d979ea83bf3cfca123f1bbd
Austin Stotts <austintstotts@gmail.com>
2019-01-04 11:55:51 -0600

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'Purchase-Proxy',
+    script: './server.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-13-59-180-205.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/petsyPurchase.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/austinStotts/Purchase-Proxy.git',
+      path: '/home/ubuntu/Purchase-Proxy',
+      'post-deploy': 'npm install; webpack; pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
