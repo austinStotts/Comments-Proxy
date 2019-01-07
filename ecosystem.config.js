fc6f12f096359ec85149b5c1148697748dea6291
Austin Stotts <austintstotts@gmail.com>
2019-01-06 18:07:18 -0600

    hopefully this works

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'ec2-13-59-180-205.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/petsyPurchase.pem',
       ref: 'origin/master',
-      repo: 'https://github.com/austinStotts/Purchase-Proxy.git',
+      repo: 'https://github.com/ByeEric/Purchase-Proxy.git',
       path: '/home/ubuntu/Purchase-Proxy',
       'post-deploy': 'npm run start && pm2 startOrRestart ecosystem.config.js'
     }
