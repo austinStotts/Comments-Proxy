133171b52606e592dca5661bf529754d4d0e083a
Austin Stotts <austintstotts@gmail.com>
2019-01-07 12:45:03 -0600

    ...

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-52-15-237-174.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/PetsyPersonal.pem',
-      ref: 'origin/master',
+      ref: 'personal',
       repo: 'git@github.com:austinStotts/Purchase-Proxy.git',
       path: '/home/ubuntu/Purchase-Proxy',
       'post-deploy': 'npm run start && pm2 startOrRestart ecosystem.config.js'
